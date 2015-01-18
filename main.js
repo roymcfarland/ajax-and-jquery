bd1fc98325e3c38897f1c447c3ee5c1c87a4f0f5
Roy McFarland <roy.mcfarland@gmail.com>
2015-01-18 08:42:51 -0700

    Updated / removed files and added two AJAX requests using $.getJSON

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
 $(document).on('ready', function() {
-  
-});
+ 
+///////////////////////////////////////////////////////
+//////////////// AJAX REQUEST NO. 1 ///////////////////
+///////////////////////////////////////////////////////
+
+  $.getJSON('data/employees.json', function (data) {
+    var statusHTML = '<ul class="bulleted">';
+    $.each(data,function (index, employee) {
+      if (employee.inoffice === true) {
+        statusHTML +='<li class="in">';
+      } else {
+        statusHTML +='<li class="out">';
+      }
+      statusHTML += employee.name + '</li>';
+    });
+    statusHTML += '</ul>';
+    $('#employeeList').html(statusHTML)
+  }); // end getJSON
+
+
+
+  ///////////////////////////////////////////////////////
+  //////////////// AJAX REQUEST NO. 2 ///////////////////
+  ///////////////////////////////////////////////////////
+
+  $.getJSON('data/rooms.json', function (data) {
+    var statusHTML = '<ul class="rooms">';
+    $.each(data,function (index, room) {
+      if (room.available === true) {
+        statusHTML +='<li class="empty">';
+      } else {
+        statusHTML +='<li class="full">';
+      }
+      statusHTML += room.room + '</li>';
+    });
+    statusHTML += '</ul>';
+    $('#roomList').html(statusHTML)
+  }); // end getJSON
+
+
+}); // end ready
